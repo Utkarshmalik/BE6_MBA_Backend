bf00f837b2dbb761a4cd1442777c3f5e6a21e134
utmalik <utmalik@amazon.com>
2023-01-23 22:33:58 +0530

    adding movies in a theatre

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -38,3 +38,80 @@ exports.getAllTheatres = async (req,res)=>{
     const theatres =  await Theatre.find(queryObj);
     res.status(200).send(theatres);
 }
+
+
+exports.getTheatre = async (req, res)=> {
+    
+    const id = req.params.id;
+
+    const theatre = await Theatre.findOne({
+        _id:id
+    })
+
+    res.status(200).send(theatre);
+}
+
+exports.deleteTheatre = async (req,res)=>{
+
+    await Theatre.deleteOne({
+        _id:req.params.id
+    });
+    
+    res.status(200).send({message:`Successfully deleted theatre with id:${req.params.id}`});
+}
+
+exports.updateTheatre = async (req,res) =>{
+
+    const id=req.params.id;
+
+    const savedTheatre = await Theatre.findOne({_id:req.params.id});
+
+    if(!savedTheatre){
+        return res.status(400).send({message:"Theatre doesn't exists"});
+    }
+
+    //update the theatre details 
+
+    savedTheatre.name = req.body.name ? req.body.name : savedTheatre.name;
+    savedTheatre.description = req.body.description ? req.body.description : savedTheatre.description;
+    savedTheatre.city = req.body.city ? req.body.city : savedTheatre.city;
+    savedTheatre.pinCode = req.body.pinCode ? req.body.pinCode : savedTheatre.pinCode;
+
+    const updatedTheatre = await savedTheatre.save();
+
+    res.status(200).send(updatedTheatre);
+
+}
+
+exports.addMoviesToTheatre = async (req,res) => {
+
+    const theatreId= req.params.theatreId;
+
+
+    const savedTheatre = await Theatre.findOne({_id:theatreId});
+
+    if(!savedTheatre){
+        return res.status(400).send({message:"Theatre doesn't exists"});
+    }
+
+    const movieIds = req.body.movies;
+
+    if(req.body.insert===true){
+        console.log("inside insert");
+    movieIds.forEach(movieId => {
+        savedTheatre.movies.push(movieId);
+    })}
+    else if(req.body.delete===true){
+
+        savedMovieIds = savedTheatre.movies.filter((movieId)=>{
+             return !movieIds.includes(movieId.toString());
+        })
+    savedTheatre.movies=savedMovieIds;
+    }
+
+
+    const updatedTheatre = await savedTheatre.save();
+    return res.status(200).send(updatedTheatre);  
+
+   
+}
--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
 
     app.post("/mba/api/v1/theatres", theatreController.createTheatre);
     app.get("/mba/api/v1/theatres", theatreController.getAllTheatres);
+    app.get("/mba/api/v1/theatres/:id", theatreController.getTheatre);
+    app.put("/mba/api/v1/theatres/:id", theatreController.updateTheatre);
+    app.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
+    app.put("/mba/api/v1/theatres/:theatreId/movies", theatreController.addMoviesToTheatre)
 }
